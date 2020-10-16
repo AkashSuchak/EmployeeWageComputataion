415a28f84c907dd57c44e0116526759b9c5b475d
AkashSuchak <suchak.akash@gmail.com>
2020-10-16 23:40:23 +0530

    Wage : Total working hours or Days is reached for a Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,34 +1,29 @@
 #! /bin/bash
 
 #Author : Akash Suchak
-#Calculation of FullTime and PartTime Employee Monthly Wage Using CASE statement
+#Calculate Wages till a condition of total working hours or days is reached for a month
 
 #Assign 0, 1 and 2 from RANDOM
 isCheck=$((RANDOM%3))
+
+#Assign Values for wage
 wagePerHour=20
 fullDayHours=8
 halfDayHours=4
-
-#calulation of Day Wage
-fullTimeWage=$((wagePerHour * fullDayHours))
-partTimeWage=$((wagePerHour * halfDayHours))
-
-#Calculation of Montly Wage
 workingDays=20
-monthlyFullTime=$((workingDays * fullTimeWage ))
-monthlyPartTime=$((workingDays * partTimeWage))
-#Wage Of Full Time and PartTime Employee
-case $isCheck in
-	0)
-		echo "FullTime Employee"
-		echo "Wage is : " $monthlyFullTime
-		;;
+totalWorkingHours=0
 
- 	1)
-		echo "PartTime Employee"
-		echo "Wage is : " $monthlyPartTime
-		;;
-	*)
-		echo "Employee is Absent"
-		;;
-esac
+#Wage Untill TotalWorkingHours Or Days is reached
+for (( i=1; i<=$workingDays; i++ ))
+do
+	check=$((RANDOM%3))
+	if [ $check -eq 0 ]; then
+		totalWorkingHours=$(("$totalWorkingHours" + "$fullDayHours"))
+	elif [ $check -eq 1 ]; then
+		totalWorkingHours=$(("$totalWorkingHours" + "$halfDayHours"))
+	else
+		totalWorkingHours=$(("$totalWorkingHours" + 0))
+	fi
+done
+totalMonthlyWage=$(("$totalWorkingHours" * "wagePerHour"))
+echo "working Hours : $totalWorkingHours and Total Wage : $totalMonthlyWage"
