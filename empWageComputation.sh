6660cded0b6b9e1231ea7680b90829f9b9efedfb
AkashSuchak <suchak.akash@gmail.com>
2020-10-17 00:25:03 +0530

    Refactor code with Fuction

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,29 +1,34 @@
 #! /bin/bash
 
 #Author : Akash Suchak
-#Calculate Wages till a condition of total working hours or days is reached for a month
+#Refactor code with Function
 
-#Assign 0, 1 and 2 from RANDOM
-isCheck=$((RANDOM%3))
+function wage(){
+	#Assign Values for wage
+	wagePerHour=20
+	fullDayHours=8
+	halfDayHours=4
+	workingDays=$1
+	totalWorkingHours=0
 
-#Assign Values for wage
-wagePerHour=20
-fullDayHours=8
-halfDayHours=4
-workingDays=20
-totalWorkingHours=0
+	#Wage Untill TotalWorkingHours Or Days is reached
+	for (( i=1; i<=$workingDays; i++ ))
+	do
+		check=$((RANDOM%3))
+		if [ $check -eq 0 ]; then
+			totalWorkingHours=$(("$totalWorkingHours" + "$fullDayHours"))
+		elif [ $check -eq 1 ]; then
+			totalWorkingHours=$(("$totalWorkingHours" + "$halfDayHours"))
+		fi
+			if [ "$totalWorkingHours" -ge 100 ]; then
+				totalWorkingHours=100
+			break
+		fi
+	done
 
-#Wage Untill TotalWorkingHours Or Days is reached
-for (( i=1; i<=$workingDays; i++ ))
-do
-	check=$((RANDOM%3))
-	if [ $check -eq 0 ]; then
-		totalWorkingHours=$(("$totalWorkingHours" + "$fullDayHours"))
-	elif [ $check -eq 1 ]; then
-		totalWorkingHours=$(("$totalWorkingHours" + "$halfDayHours"))
-	else
-		totalWorkingHours=$(("$totalWorkingHours" + 0))
-	fi
-done
-totalMonthlyWage=$(("$totalWorkingHours" * "wagePerHour"))
-echo "working Hours : $totalWorkingHours and Total Wage : $totalMonthlyWage"
+	#Display Total Working Hours with Total Wage
+	totalMonthlyWage=$(("$totalWorkingHours" * "wagePerHour"))
+	echo "working Hours : $totalWorkingHours and Total Wage : $totalMonthlyWage"
+}
+#calling Function
+wage 20
