43496e67edcf2400b849d04e51909197431baa36
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2020-10-17 01:00:58 +0530

    Update empWageComputation.sh
    Remove Conflicts

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,9 @@
 #! /bin/bash
 
 #Author : Akash Suchak
-<<<<<<< HEAD
 #Calculate Monthly Wage Untill TotalWorkingHours Or Days is reached
 
-=======
 #Refactor code with Function
-
 function wage(){
 	#Assign Values for wage
 	wagePerHour=20
@@ -14,7 +11,6 @@ function wage(){
 	halfDayHours=4
 	workingDays=$1
 	totalWorkingHours=0
->>>>>>> UC7-RefactorCodeWithFunction
 
 	#Wage Untill TotalWorkingHours Or Days is reached
 	for (( i=1; i<=$workingDays; i++ ))
@@ -31,30 +27,9 @@ function wage(){
 		fi
 	done
 
-<<<<<<< HEAD
-#Wage Untill TotalWorkingHours Or Days is reached
-for (( i=1; i<=$workingDays; i++ ))
-do
-	check=$((RANDOM%3))
-	if [ $check -eq 0 ]; then
-		totalWorkingHours=$(("$totalWorkingHours" + "$fullDayHours"))
-	elif [ $check -eq 1 ]; then
-		totalWorkingHours=$(("$totalWorkingHours" + "$halfDayHours"))
-	fi
-		if [ "$totalWorkingHours" -ge 100 ]; then
-			totalWorkingHours=100
-		break
-	fi
-done
-
-#Display Total Working Hours with Total Wage
-totalMonthlyWage=$(("$totalWorkingHours" * "wagePerHour"))
-echo "working Hours : $totalWorkingHours and Total Wage : $totalMonthlyWage"
-=======
 	#Display Total Working Hours with Total Wage
 	totalMonthlyWage=$(("$totalWorkingHours" * "wagePerHour"))
 	echo "working Hours : $totalWorkingHours and Total Wage : $totalMonthlyWage"
 }
 #calling Function
 wage 20
->>>>>>> UC7-RefactorCodeWithFunction
